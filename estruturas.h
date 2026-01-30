f8a7b8c774fdeccec102977bd9dbb4d027863d23
sauhangit-ctrl <sauhangit@gmail.com>
2026-01-30 13:52:52 -0300

    correçoes na pasta da estrutura

--- estruturas.h
+++ estruturas.h
@@ -1,21 +1,30 @@
 #ifndef ESTRUTURAS_H
 #define ESTRUTURAS_H
 
+
+typedef struct _ItemCarrinho ItemCarrinho;
+typedef struct _Produtos Produtos;
+typedef struct _Clientes Clientes;
+
 typedef struct _Clientes {
-    int CPF, telefone, dt_nascimento;
+    char  CPF[15], telefone[20], dt_nascimento[11];
     char nome[101];
     char email[101];
     struct _Clientes* proximo;
+    ItemCarrinho* carrinho;
+
 } Clientes;
 
 typedef struct _Produtos {
-    int codigo, preco, qtde;
+    int codigo, qtde;
+    float preco;
     char nome[101];
     struct _Produtos* proximo;
 } Produtos;
 
 typedef struct _ItemCarrinho{
-    int codigo, qtde, preco;
+    int codigo, qtde;
+    float preco;
     char nome[101];
     struct _ItemCarrinho* proximo;
 } ItemCarrinho;
